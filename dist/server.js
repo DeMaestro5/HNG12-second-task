c2f5c95d88a2b9f0404370e07ebf953602676919
stephen <osstephen70@gmail.com>
2025-02-05 10:55:23 +0100

    fixes

--- dist/server.js
+++ dist/server.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const PORT = process.env.PORT || 3000;
-const server = app_1.default.listen(PORT, () => {
+app_1.default.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-exports.default = server;
